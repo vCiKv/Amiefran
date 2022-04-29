c4e12af8ce36a1a9ddaf8846a7283ae2c7b9a808
vCiKv <igwebuikechidubem@gmail.com>
2022-04-29 22:52:34 +0100

    added gallery

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['res.cloudinary.com','www.istockphoto.com'],
+    domains: ['res.cloudinary.com','www.istockphoto.com','drive.google.com','google.com','lh3.googleusercontent.com'],
   },
 }
